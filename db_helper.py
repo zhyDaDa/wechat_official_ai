e01dfb688c956c39943082ac5c5b3b32437e05a7
zhyDaDa <zhydada_nightmare@outlook.com>
2025-09-24 08:25:02 +0800

    数据库

---
+++ db_helper.py
@@ -0,0 +1,62 @@
+import pymysql
+
+
+class DBHelper:
+    def __init__(self):
+        self.conn = pymysql.connect(
+            host="127.0.0.1",
+            port=3306,
+            user="root",
+            password="mysql_t7kHiH",
+            database="1Panel-mysql-vSVY",
+            charset="utf8mb4",
+        )
+        self.cursor = self.conn.cursor()
+
+    def create_table(self):
+        sql = """
+        CREATE TABLE IF NOT EXISTS msg_history (
+            id BIGINT PRIMARY KEY AUTO_INCREMENT,
+            to_user VARCHAR(64),
+            from_user VARCHAR(64),
+            create_time VARCHAR(32),
+            msg_type VARCHAR(16),
+            msg_id VARCHAR(64),
+            content TEXT,
+            pic_url VARCHAR(256),
+            media_id VARCHAR(128)
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+        """
+        self.cursor.execute(sql)
+        self.conn.commit()
+
+    def insert_msg(self, to_user, from_user, create_time, msg_type, msg_id, content=None, pic_url=None, media_id=None):
+        sql = """
+        INSERT INTO msg_history (to_user, from_user, create_time, msg_type, msg_id, content, pic_url, media_id)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+        """
+        self.cursor.execute(
+            sql, (to_user, from_user, create_time, msg_type, msg_id, content, pic_url, media_id)
+        )
+        self.conn.commit()
+
+    def insert_image_msg(self, to_user, from_user, create_time, msg_type, msg_id, pic_url, media_id):
+        self.insert_msg(
+            to_user=to_user,
+            from_user=from_user,
+            create_time=create_time,
+            msg_type=msg_type,
+            msg_id=msg_id,
+            content=None,
+            pic_url=pic_url,
+            media_id=media_id
+        )
+
+    def get_msgs(self, limit=100):
+        sql = "SELECT * FROM msg_history ORDER BY id DESC LIMIT %s"
+        self.cursor.execute(sql, (limit,))
+        return self.cursor.fetchall()
+
+    def close(self):
+        self.cursor.close()
+        self.conn.close()
